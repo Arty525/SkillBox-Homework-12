261e77b5cf8c64394f8bc84ff5b60cc2805c0512
Andrey <89473462+Arty525@users.noreply.github.com>
2021-11-17 15:29:55 +0300

    ex3
    ex3

---
+++ ex3/ex3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float numbers[15];
+	float number;
+
+	cout << "Input 15 numbers: " << endl;
+	//Input values
+	for (int i = 0; i != 15; ++i) {
+		cout << i + 1 << ") ";
+		cin >> number;
+		if (i > 0 && number >= numbers[i - 1]) {
+			numbers[i] = number;
+		}
+		else if (i == 0) {
+			numbers[i] = number;
+		}
+		else {
+			for (int x = i;number < numbers[x - 1]; --x) {
+				numbers[x] = numbers[x - 1];
+				numbers[x - 1] = number;
+			}
+		}
+	}
+	//Searching minimal value
+	for (int i = 0; i != 15; ++i) cout << numbers[i] << endl;
+}
