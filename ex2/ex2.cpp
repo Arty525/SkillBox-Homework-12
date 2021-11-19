b3bd0fc668069687f1fc0932fe62f0a3ba2d95eb
Andrey <89473462+Arty525@users.noreply.github.com>
2021-11-19 08:26:56 +0300

    Add files via upload

--- ex2/ex2.cpp
+++ ex2/ex2.cpp
@@ -4,14 +4,17 @@ using namespace std;
 int main() {
     int numbers[15] = {114,111,106,107,108,105,115,108,110,109,112,113,116,117,118};
     int repeat = 0;
+    int startPos = numbers[0];
+    int sort[15];
     for (int i = 0; i < 15; ++i) {
-        for (int n = i+1; n < 15; ++n) {
-            if (numbers[n] == numbers[i]) {
-                repeat = numbers[n];
-                break;
-            }
+        if (numbers[i] < startPos) {
+            startPos = numbers[i];
         }
-        if (repeat != 0) break;
+    }
+    for (int i = 0; i < 15; ++i) {
+        int x = numbers[i] - startPos;
+        if (sort[x] == numbers[i]) repeat = numbers[i];
+        else sort[x] = numbers[i];
     }
     cout << "Repeated number: " << repeat << endl;
 }
